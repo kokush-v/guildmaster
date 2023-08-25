953acb09ea644ea61fb9bb3a508c388ccd955a9b
cam-per <minkotaras@gmail.com>
2023-08-26 01:05:40 +0300

    discord.js as local lib

---
+++ libs/discord.js/src/client/websocket/handlers/READY.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { REST, Routes } = require('discord.js');
+const ClientApplication = require('../../../structures/ClientApplication');
+let ClientUser;
+
+module.exports = (client, { d: data }, shard) => {
+  if (client.user) {
+    client.user._patch(data.user);
+  } else {
+    ClientUser ??= require('../../../structures/ClientUser');
+    client.user = new ClientUser(client, data.user);
+    client.users.cache.set(client.user.id, client.user);
+  }
+
+  for (const guild of data.guilds) {
+    guild.shardId = shard.id;
+    client.guilds._add(guild);
+  }
+
+  if (client.application) {
+    client.application._patch(data.application);
+  } else {
+    if(!data.application) {
+      data.application = {
+        id: process.env.DISCORD_CLIENT_ID ?? '',
+        name: 'undefined'
+      }
+    }
+    client.application = new ClientApplication(client, data.application);
+  }
+
+  shard.checkReady();
+};
